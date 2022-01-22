1b05384175be580cc618cfa727af4130dc085f26
Poramee Chansuksett <poramee.chansuksett@gmail.com>
2022-01-22 18:17:40 +0700

    [FCA-108] Make Google ML Kit run on the example file

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:io';
 
+import 'package:camera/camera.dart';
 import 'package:flutter/services.dart';
 import 'package:fca_pose_validation/fca_pose_validation.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
+import 'pose_detector_view.dart';
 
-void main() {
+List<CameraDescription> cameras = [];
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
   runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -235,16 +243,51 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: Text('Google ML Kit Demo App'),
+          centerTitle: true,
+          elevation: 0,
         ),
-        body: Center(
-            child: Column(children: [
-          const Text('fca_pose_validation test'),
-          TextButton(
-            child: const Text('TEST', style: TextStyle(fontSize: 20.0)),
-            onPressed: poseValidationTest,
-          )
-        ])),
+        body: SafeArea(
+          child: Center(
+            child:
+              PoseDetectorView()
+              // TextField()
+            ),
+          ),
+        )
+      );
+  }
+
+}
+
+class CustomCard extends StatelessWidget {
+  final String _label;
+  final Widget _viewPage;
+  final bool featureCompleted;
+
+  const CustomCard(this._label, this._viewPage,
+      {this.featureCompleted = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.only(bottom: 10),
+      child: ListTile(
+        tileColor: Theme.of(context).primaryColor,
+        title: Text(
+          _label,
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        onTap: () {
+          if (Platform.isIOS && !featureCompleted) {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: const Text(
+                    'This feature has not been implemented for iOS yet')));
+          } else
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => _viewPage));
+        },
       ),
     );
   }

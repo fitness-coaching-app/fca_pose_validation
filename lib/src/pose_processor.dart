0f0eefb4fed208595a3447b12c21c4942fb32f64
Poramee Chansuksett <poramee.chansuksett@gmail.com>
2022-03-04 21:56:32 +0700

    :sparkles: Implement PoseProcessorError

--- lib/src/pose_processor.dart
+++ lib/src/pose_processor.dart
@@ -1,5 +1,21 @@
 import 'package:fca_pose_validation/src/angle_calculator.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
+import 'exercise_controller.dart';
+
+enum PoseProcessorType{
+  angle,
+  touch
+}
+
+class PoseProcessorError{
+  final PoseLandmarkType landmark;
+  final PoseProcessorType from;
+  final Definition rule;
+  final double actualValue;
+
+  PoseProcessorError(this.landmark, this.from, this.rule, this.actualValue);
+}
+
 
 class PoseProcessor {
   AngleCalculator angle = AngleCalculator();

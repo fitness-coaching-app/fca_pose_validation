34d3aea205b63af395e5c92f672878ab15082760
Poramee Chansuksett <poramee.chansuksett@gmail.com>
2022-03-05 15:10:16 +0700

    :sparkles: Create TouchChecker class for further development

--- lib/src/pose_processor.dart
+++ lib/src/pose_processor.dart
@@ -1,6 +1,7 @@
-import 'package:fca_pose_validation/src/angle_calculator.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
-import 'exercise_controller.dart';
+import 'exercise_definition.dart';
+import 'angle_calculator.dart';
+import 'touch_checker.dart';
 
 enum PoseProcessorType{
   angle,
@@ -19,8 +20,10 @@ class PoseProcessorError{
 
 class PoseProcessor {
   AngleCalculator angle = AngleCalculator();
+  TouchChecker touch = TouchChecker();
 
   void setPose(Pose pose){
     angle.setPose(pose);
+    touch.setPose(pose);
   }
 }
---
+++ lib/src/touch_checker.dart
@@ -0,0 +1,11 @@
+import 'package:google_ml_kit/google_ml_kit.dart';
+
+class TouchChecker{
+  late Pose _pose;
+
+  void setPose(Pose pose){
+    _pose = pose;
+  }
+
+
+}
